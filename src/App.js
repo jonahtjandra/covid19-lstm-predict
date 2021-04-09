407b3200b91a526010674343adae1ec8d1bf0bbe
jonah <jonahlt2@illinois.edu>
2021-04-09 22:54:28 +0700

    restructure and fixed react app bug

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-      super(props);
-  }
-  render() {
-    return(
-        <h1>simulation</h1>
-    )
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Mainpage.js
+++
@@ -1,7 +0,0 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './Mainpage.css';
-
-export default class Feed extends Component {
-}
